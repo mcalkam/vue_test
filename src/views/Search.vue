795f369e8e781c0f07a46550c2616b291af15d68
mcalkam <52129070+mcalkam@users.noreply.github.com>
2021-08-04 08:51:56 +0200

    Connect to NASAA API

---
+++ src/views/Search.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Szukaj </label>
+      <input
+        id="search"
+        name="search"
+        v-model="searchValue"
+        @input="handleInput"
+      />
+    </div>
+    <ul>
+      <li v-for="item in results" v-bind:key="item.data[0].nasa_id">
+        <p>{{item.data[0].description}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+
+const API = 'https://images-api.nasa.gov/search';
+export default {
+  name: 'Search',
+  data() {
+    return {
+      // musi być początkowa wartość przy v-model
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    handleInput: debounce(function () {
+    // console.log(this.searchValue);
+      axios
+        .get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          console.log(response.data.collection.items);
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+}
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+}
+label {
+  font-family: Montserrot, sans-serif;
+}
+input {
+  height: 50px;
+  border: 0;
+  border-bottom: 1px solid black;
+}
+</style>
